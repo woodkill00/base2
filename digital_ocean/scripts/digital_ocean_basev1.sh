00a20c4bc0adaf70395e075a06fc4085590e4b20
woodkill00 <thejunkmailvoid@gmail.com>
2025-12-09 21:34:55 -0700

    chore: clean up and finalize digital_ocean_basev1.sh for reliable droplet setup

--- digital_ocean/scripts/digital_ocean_basev1.sh
+++ digital_ocean/scripts/digital_ocean_basev1.sh
@@ -15,26 +15,8 @@ git clone https://github.com/woodkill00/base2.git /opt/base2
 ufw allow 22
 ufw allow 80
 ufw allow 443
-#!/bin/bash
-set -e
-
-echo "User data script started at $(date)" | tee -a /var/log/cloud-init-output.log
-
-# Install git
-apt-get update -y
-apt-get install -y git
-
-# Clone repo to /opt/base2
-mkdir -p /opt/base2
-git clone https://github.com/woodkill00/base2.git /opt/base2
-
-# Set up UFW
-ufw allow 22
-ufw allow 80
-ufw allow 443
 ufw --force enable
 
 touch /var/log/do_base_complete
 
 echo "User data script completed at $(date)" | tee -a /var/log/cloud-init-output.log
-log "**********TURNING OFF APACHE**********"
