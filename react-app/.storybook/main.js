abdb4ccaef08ca14a598422ad92cf686310c9681
woodkill00 <thejunkmailvoid@gmail.com>
2025-12-30 18:44:18 -0700

    chore(storybook): add CRA preset and @storybook/test; fix Storybook build
    - Add @storybook/preset-create-react-app (v8.6.15) to addons
    - Install @storybook/test (v8.6.15) to satisfy peer import
    - Build now succeeds and outputs react-app/storybook-static

--- react-app/.storybook/main.js
+++ react-app/.storybook/main.js
@@ -1,8 +1,13 @@
 module.exports = {
-  stories: ["../src/**/*.stories.@(js|jsx)"],
-  addons: ["@storybook/addon-essentials"],
+  stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-essentials",
+    // Ensure CRA Babel/Webpack config is applied for JSX/TSX
+    "@storybook/preset-create-react-app",
+  ],
   framework: {
-    name: "@storybook/react",
-    options: {}
+    // Use the Webpack 5 framework package for Storybook 8
+    name: "@storybook/react-webpack5",
+    options: {},
   },
 };
