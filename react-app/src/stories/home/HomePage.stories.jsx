c248e5f6308079987fd50878b1cc1fa9022e0230
woodkill00 <thejunkmailvoid@gmail.com>
2026-01-16 13:32:03 -0700

    fix(storybook): satisfy lint for HomePage story

--- react-app/src/stories/home/HomePage.stories.jsx
+++ react-app/src/stories/home/HomePage.stories.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 
 import Home from '../../pages/Home';
 
-export default {
+const meta = {
   title: 'Pages/Home',
   component: Home,
   parameters: {
     layout: 'fullscreen',
   },
 };
+
+export default meta;
 
 export const Default = () => (
   <MemoryRouter>
